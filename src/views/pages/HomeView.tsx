c691f2190dc09b783f9a1cf2d18d97bb3fbcdf49
EngMohamed95 <eng.mhamed99@gmail.com>
2026-04-29 11:08:36 +0300

    feat: implement home page with bullion search and i18n support

--- src/views/pages/HomeView.tsx
+++ src/views/pages/HomeView.tsx
@@ -7,7 +7,7 @@ import { useAppController, useBullionController } from '../../controllers/appCon
 import coverImg from '../../../كوفرر.webp';
 import img1 from '../../../1.webp';
 import img2 from '../../../2.webp';
-import img3 from '../../../3.webp';
+import img3 from '../../../سبايك.webp';
 import img4 from '../../../4.webp';
 
 export default function HomeView() {
